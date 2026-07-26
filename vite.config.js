4ffd670305cf3506c12ea64df7cd6beac77fc0e0
AnthonyRQC <aqcapquique02@gmail.com>
2026-07-26 11:29:33 -0400

    pre-sprint10: snapshot before MySQL migration

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
     server: {
         host: '0.0.0.0',
         hmr: {
-            host: '192.168.1.15'
+            host: '192.168.1.13'
         },
         cors: true,
     },
